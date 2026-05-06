fed538896fdb2cec1271ce9b3405a6af00cb935f
solusi consulting group <kurniawan@petalmail.com>
2026-05-06 11:52:43 +0700

    Remove debug route and restore heartbeat route

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MonitoringController;
 
 Route::post('/v1/heartbeat', [MonitoringController::class, 'handleHeartbeat']);
-
-Route::get('/v1/debug-log-xyz', function () {
-    $logFile = storage_path('logs/laravel.log');
-    if (file_exists($logFile)) {
-        // Return the last 50 lines
-        $lines = file($logFile);
-        $lastLines = array_slice($lines, -50);
-        return response()->json(['log' => implode("", $lastLines)]);
-    }
-    return response()->json(['log' => 'No log file found']);
-});
